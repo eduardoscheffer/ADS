87bab2e124531db2c2eaa44790cdf11c58dba568
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-05-26 19:59:20 -0300

    update_aluno_id

--- Algoritmos/crud_grupo4/atualiza_aluno_id.py
+++ Algoritmos/crud_grupo4/atualiza_aluno_id.py
@@ -8,36 +8,53 @@ alunos = [
     
 ]
 
-while True:
-    try:
-       id_busca = input('Qual o ID deseja atualiza? ')
-       id_busca = int(id_busca)
-       if id_busca >= 0:
-           break
-    except:
-        print('ID dever ser um numero inteiro valido maior que 0. Tente novamente')
-
-# pra cada aluno na lista de alunos
-for aluno in alunos:
-    if aluno[0] == id_busca:
+def atualiza_aluno():
+    # pega as informacoes do usuario e valida elas
+    while True:
         try:
-            novo_nome = input('Qual o novo nome do aluno? ')
-            nova_idade = int(input('Qual a nova idade do aluno? '))
-            if nova_idade < 0:
-                break
+            # remove qualquer espaço em branco antes ou depois do input do usuario:
+            id_busca = int(input('Qual o ID deseja atualizar? ').strip())
+            if id_busca > 0:
+                break # se deu tudo certo sai do loop e vai pro for in na lista de alunos
+            else:
+                print('O ID começa em 1')
+        # trata algum eventual erro
         except:
-            print('Nome deve conter apenas caracteres validos e idade deve ser um numero inteiro maior que 0')
-            break
-        
-        aluno[0] = id_busca
-        aluno[1] = novo_nome
-        aluno[2] = nova_idade
-        print('Atualizado com sucesso')
-        break
-    print('Aluno não encontrado')
-    break
-
-# print(aluno)
-for aluno in alunos:
-    print(aluno)
+            print('ID deve ser um número inteiro válido maior que 0. Tente novamente')
     
+    encontrado = False # variavel que controla se o ID foi encontrado & atualizado ou nao
+    # pra cada aluno na lista de alunos:
+    for aluno in alunos:
+        if aluno[0] == id_busca: # se o ID passado pelo usuário for encontrado na lista entra na validação e depois pra atualização
+            while True:
+                try:
+                    # pega os dados do usuario                limpa qualquer espaço em branco
+                    novo_nome = str(input('Qual o novo nome do aluno? ')).strip() # input do usuario
+                    nova_idade = int(input(('Qual a nova idade do aluno? ')).strip()) # input do usuario
+
+                    # checa se os dados são válidos
+                    if novo_nome and nova_idade > 0: # se o nome for passado e a idade for positiva:
+                        break # quebra do loop e vai pra atualização dos dados
+                    else:
+                        print('Idade não pode ser menor que 0 e nome não pode ficar vazio') 
+                        continue # volta pro começo do loop
+                except:
+                    print('Nome deve conter apenas caracteres válidos e idade deve ser um número inteiro maior que 0') # caso alguma coisa no try der errada, cai aqui e volta pro começo do loop
+
+            # ---- Atualização dos dados do Aluno ----- #            
+            aluno[0] = id_busca # atualiza o id do aluno escolhido
+            aluno[1] = novo_nome # atualiza o nome do aluno
+            aluno[2] = nova_idade # atualiza a idade do aluno
+            print('Aluno atualizado com sucesso!')
+            encontrado = True
+            break # quebra o loop e termina a função
+        
+    # --- Caso o aluno (ID) não for encontrado
+    if not encontrado:
+        print('Aluno não encontrado')
+        
+
+
+atualiza_aluno() # chama a função pra ser executada
+print(alunos) # printa a nova lista de alunos atualizada
+
