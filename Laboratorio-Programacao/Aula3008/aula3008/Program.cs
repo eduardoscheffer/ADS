09bff5ae14a05d9edac22b5b37cba6bbe9ac91e7
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-08-30 21:31:35 -0300

    Aula 06 Lab Prog

---
+++ Laboratorio-Programacao/Aula3008/aula3008/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program 
+{
+    static void Main(string[] args)
+    {
+        // for (int i = 0; i <= 10; i++)
+        // {   
+        //     Console.WriteLine($"5 X {i} = {5 * i}");
+        // }
+
+        int total = 0;
+        for (int i = 1; i <= 10; i++)
+        {
+            total += i;
+        }
+        Console.WriteLine(total);
+
+    }
+}
