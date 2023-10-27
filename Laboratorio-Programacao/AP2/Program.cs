36a75558224a762c38d7d548733abd5e927be918
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-10-27 13:13:02 -0300

    update TDE2 Laboratorio-Programacao

--- Laboratorio-Programacao/AP2/Program.cs
+++ Laboratorio-Programacao/AP2/Program.cs
@@ -3,41 +3,37 @@ namespace AP2;
 internal class Program
 {
     static void Main(string[] args)
-    {
-        Jogador jogador1 = new Jogador("Jogador1", "Nick1");
-        Jogador jogador2 = new Jogador("Jogador2", "Nick2");
-        Jogador jogador3 = new Jogador("Jogador3", "Nick3");
-        Jogador jogador4 = new Jogador("Jogador4", "Nick4");
-        Jogador jogador5 = new Jogador("Jogador5", "Nick5");
+    {   
+        Campeonato campeonato = new Campeonato("CS-GO CHAMPIONSHIP");
+        
+        Console.Write("Quantas equipes disputarão o campeonato? ");
+        int n = int.Parse(Console.ReadLine());
 
-        Equipe equipe1 = new Equipe("Vikings");
-        equipe1.AdicionarJogador(jogador1);
-        equipe1.AdicionarJogador(jogador2);
-        equipe1.AdicionarJogador(jogador3);
-        equipe1.AdicionarJogador(jogador4);
-        equipe1.AdicionarJogador(jogador5);
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine($"Equipe #{i}");
+            Console.Write("Nome da Equipe: ");
+            string nomeEquipe = Console.ReadLine();
+            Equipe equipe = new(nomeEquipe);
 
-        Jogador jogador6 = new Jogador("Jogador1", "Nick1");
-        Jogador jogador7 = new Jogador("Jogador2", "Nick2");
-        Jogador jogador8 = new Jogador("Jogador3", "Nick3");
-        Jogador jogador9 = new Jogador("Jogador4", "Nick4");
-        Jogador jogador10 = new Jogador("Jogador5", "Nick5");
+            for (int j = 1; j <= 5; j++)
+            {   
+                Console.Write($"Jogador{j}: ");
+                string playerName = Console.ReadLine();
+                Console.Write($"Nickname do {playerName}: ");
+                string nickname = Console.ReadLine();
+                Jogador player = new(playerName, nickname);
 
-        Equipe equipe2 = new Equipe("Rawks");
-        equipe2.AdicionarJogador(jogador6);
-        equipe2.AdicionarJogador(jogador7);
-        equipe2.AdicionarJogador(jogador8);
-        equipe2.AdicionarJogador(jogador9);
-        equipe2.AdicionarJogador(jogador10);
+                equipe.AdicionarJogador(player);
+            }
 
-        List<Equipe> equipes = new List<Equipe> {equipe1, equipe2};
+            campeonato.AdicionarEquipes(equipe);
 
-        Campeonato campeonato = new("Campeonato de CS");
-        campeonato.AdicionarEquipes(equipes.ToArray());
+        }
 
         campeonato.IniciarPartida();
 
         campeonato.Classificacao();
-        
+
     }
 }
