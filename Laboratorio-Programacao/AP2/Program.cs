029b304f038ea661e6843ab557744f9bb21199d8
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-10-27 13:24:08 -0300

    updated AP2 laboratorio-programacao

--- Laboratorio-Programacao/AP2/Program.cs
+++ Laboratorio-Programacao/AP2/Program.cs
@@ -4,36 +4,71 @@ internal class Program
 {
     static void Main(string[] args)
     {   
-        Campeonato campeonato = new Campeonato("CS-GO CHAMPIONSHIP");
-        
-        Console.Write("Quantas equipes disputarão o campeonato? ");
-        int n = int.Parse(Console.ReadLine());
 
-        for (int i = 1; i <= n; i++)
+        while (true)
         {
-            Console.WriteLine($"Equipe #{i}");
-            Console.Write("Nome da Equipe: ");
-            string nomeEquipe = Console.ReadLine();
-            Equipe equipe = new(nomeEquipe);
+            Console.WriteLine("1. Criar Campeonato");
+            Console.WriteLine("2. Sair");
+            Console.Write("Escolha uma opção: ");
 
-            for (int j = 1; j <= 5; j++)
-            {   
-                Console.Write($"Jogador{j}: ");
-                string playerName = Console.ReadLine();
-                Console.Write($"Nickname do {playerName}: ");
-                string nickname = Console.ReadLine();
-                Jogador player = new(playerName, nickname);
+            string escolha = Console.ReadLine();
 
-                equipe.AdicionarJogador(player);
+            if (escolha == "1")
+            {
+                try
+                {
+                    Console.Write("Nome do Campeonato: ");
+                    string nomeCampeonato = Console.ReadLine();
+
+                    Console.Write("Quantas equipes disputarão o campeonato? ");
+                    int n = int.Parse(Console.ReadLine());
+
+                    if (n <= 0)
+                    {
+                        throw new Exception("Número inválido de equipes.");
+                    }
+
+                    Campeonato campeonato = new Campeonato(nomeCampeonato);
+
+                    for (int i = 1; i <= n; i++)
+                    {
+                        Console.WriteLine($"Equipe #{i}");
+                        Console.Write("Nome da Equipe: ");
+                        string nomeEquipe = Console.ReadLine();
+                        Equipe equipe = new Equipe(nomeEquipe);
+
+                        for (int j = 1; j <= 5; j++)
+                        {
+                            Console.Write($"Jogador{j}: ");
+                            string playerName = Console.ReadLine();
+                            Console.Write($"Nickname do {playerName}: ");
+                            string nickname = Console.ReadLine();
+                            Jogador player = new Jogador(playerName, nickname);
+
+                            equipe.AdicionarJogador(player);
+                        }
+
+                        campeonato.AdicionarEquipes(equipe);
+                    }
+
+                    campeonato.IniciarPartida();
+                    campeonato.Classificacao();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Erro: {ex.Message}");
+                }
             }
-
-            campeonato.AdicionarEquipes(equipe);
-
+            else if (escolha == "2")
+            {
+                Console.WriteLine("Saindo do programa...");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Opção inválida. Por favor, escolha uma opção válida.");
+            }
         }
-
-        campeonato.IniciarPartida();
-
-        campeonato.Classificacao();
 
     }
 }
